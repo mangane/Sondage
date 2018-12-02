892fb49e804d45b822a2f4a71f2eccbadab08f55
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 21:43:21 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,17 +10,8 @@ client.on('message', message => {
     	message.reply('**pong!**');
   	}
     if (message.content === 'test') {
-        message.reply($1);
+        message.channel.send(args.join);
     }  
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-}
 });
 
 // THIS  MUST  BE  THIS  WAY
