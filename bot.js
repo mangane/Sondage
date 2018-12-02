3fd638540ed6d1b933af74c5f056609c30cec953
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 22:02:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,19 @@ client.on('message', message => {
     if (message.content === '!Voca') {
         message.channel.send('attention a ton vocabulaire member.mention');
     }  
+if(message.author.bot) { return; }
+
+if(!message.mentions.members.array()[2]){ return; }
+
+message.author.send(`Please stop mass mentioning.`)
+
+numberofmentions++;
+
+if(numberofmentions > 3){
+  message.member.kick();
+  message.author.send(`You've Been Kicked From ${message.guild} for Spamming Mentions.`);
+  message.channel.send(`User ${message.author.username}#${message.author.discriminator} Has Been Kicked For Mass Mentinoing`);
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
