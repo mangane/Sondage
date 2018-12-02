3769c96d1edd6a7c6a193a339d691bcfc3b17f3e
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 11:16:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,30 @@ client.on('message', message => {
     if (message.content === '!ping') {
     	message.reply('pong');
   	}
+const Discord = require("discord.js");
+
+exports.run = (client, message, args, level) => {
+
+  let question = args.slice(0).join(" ");
+
+  if (args.length === 0)
+  return message.reply('**Invalid Format:** `!Poll <Question>`')
+
+  const embed = new Discord.RichEmbed()
+  .setTitle("A Poll Has Been Started!")
+  .setColor("#5599ff")
+  .setDescription(`${question}`)
+  .setFooter(`Poll Started By: ${message.author.username}`, `${message.author.avatarURL}`)
+
+  message.channel.send({embed})
+  .then(msg => {
+    msg.react('👍')
+    msg.react('👎')
+    msg.react('🤷')
+  })
+  .catch(() => console.error('Emoji failed to react.'));
+
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
