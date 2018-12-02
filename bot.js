2036154f142acd0e7c8fd4b58e63c616cd1a0683
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 11:26:25 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,9 @@ client.on('message', message => {
     if (message.content === '!ping') {
     	message.reply('pong');
   	}
-const config = require('../config.json');
+    if (message.content === '!roue'){
+        message.reply('message.content');}
 
-exports.run = (client, message, args) => {
-
-return message.reply(`\n${config.prefix}Help - Displays Help Menu\n${config.prefix}Poll <Question>\n${config.prefix}Prefix <New Prefix> - Sets New Prefix. **[BOT OWNER ONLY${config.prefix}]**`)
-
-}
 });
 
 // THIS  MUST  BE  THIS  WAY
