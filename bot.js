6f91ac410dce80c27f82baa90f9adeb230634320
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 21:06:34 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,20 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+const cmdName = 'lmgtfy';
+const description = 'lets me google that for you.';
+
+function execute (cmd) {
+  cmd.parameter
+  ? cmd.reply(`http://lmgtfy.com/?q=${cmd.args.join('+')}`)
+  : cmd.reply('http://lmgtfy.com/?q=how+to+use+lmgtfy');
+}
+
+module.exports = {
+  cmdName,
+  description,
+  execute,
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
