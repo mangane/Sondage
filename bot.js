aa4fad097d00e84481248c3349fbf4e2689fe095
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 20:33:57 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,20 +1,21 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+/*
+	BOT Report Discord par @raymater
+	
+	Documentation Discord.js : https://discord.js.org
+*/
 
-client.on('ready', () => {
-    console.log('I am ready!');
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+
+var token = "votre_token";
+
+var dernierAppel = new Array(); // Matrice stockant le timestamp du dernier appel de la commande + UserID l'ayant appelé
+
+bot.on("ready", () => {
+	console.log("Bot démarré");
 });
 
-client.on('message', message => {
-    if (message.content === '!ping') {
-    	message.reply('pong');
-  	}
- if(message.content === "say") {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{});
-    message.reply(sayMessage);
-}
- {
+bot.on("message", (message) => {
 	
 	/*
 		Object message :
@@ -175,8 +176,7 @@ client.on('message', message => {
 				}
 			}
 		}
-}
+	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(token);
