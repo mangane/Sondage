ed3961f9af6b1c70c04db7d13e30b3cf653e4d70
mangane <40431348+mangane@users.noreply.github.com>
2018-12-02 21:20:18 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('message', message => {
     	message.reply('pong');
   	}
     if (message.content === 'test') {
-        message.reply${cmd.args.join('');}
+        message.reply(${cmd.args.join(''));}
     }                  
 });
 
