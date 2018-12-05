c09dc907accbff5518f6c51670f619bc6e51dc84
mangane <40431348+mangane@users.noreply.github.com>
2018-12-05 16:06:03 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,7 @@ client.on("message", message => {
         message.reply(" voici la liste des commandes : ```!poll ``` ```!commandes```!clear (efface 10 messages)```"); }
       if (message.content.startsWith('!clear')) {
         message.delete(5);
+          message.delete(5);
         message.reply('Les messages on bien été effacer !'); }
        
 });
