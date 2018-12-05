59777dfe09e2af9bfa9afca1c64db130f16d28a1
mangane <40431348+mangane@users.noreply.github.com>
2018-12-05 14:20:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on("message", message => {
         message.reply("change de vocabulaire merci!"); }
     if (message.content.startsWith('!commandes')) {
         message.delete(1);
-        message.reply(" voici la liste des commandes : ```!poll ``` ```!commandes```"); }
+        message.reply(" voici la liste des commandes : ```!poll ``` ```!commandes```!clear (efface 10 messages)```"); }
     
 	if (message.content.startsWith('!clear')) {
         message.delete(10);
