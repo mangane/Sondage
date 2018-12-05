ca99e4ad0f243c5476dec80dfc99ca6346d76ab8
mangane <40431348+mangane@users.noreply.github.com>
2018-12-05 13:33:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,7 @@ client.on("message", message => {
     await message.react("✅")
     await message.react("❌")
     await message.react("🤷")
+      message.delete(1)
     });
   }
     if (message.content.startsWith('salope')) {
