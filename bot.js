01ff6fab4963d2a4673bbb61753e70b1ece75026
mangane <40431348+mangane@users.noreply.github.com>
2019-03-09 04:19:03 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,17 +14,7 @@ client.on(`message`, message =>{
     if(message.content.startsWith("help")) {
         message.channel.send("Salut, taper !help sûr #commande pour voir les commandes de Modérateur[Bêta]");
         }
-    if(message.content.startsWith(prefix + "help")) {
-        (message.guild.reateChannel('ticket-' + (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4), 0, 'New ticket', bot.config.category).then((channel) => {
-          r.table('tickets').insert({
-            id: msg.author.id,
-            case: (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4),
-            channel: channel.id
-          }).run();
-          channel.createMessage({
-            embed: {
-              description: 'ID: ' + (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4) + '\nMessage: ' + (args.length > 0 ? args.join(' ') : 'None')
-            }
+    
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
