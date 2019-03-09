4dfcdab57a8d3ced24efa0d4fd4eed0a4e1db71b
mangane <40431348+mangane@users.noreply.github.com>
2019-03-09 04:17:30 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,16 +14,17 @@ client.on(`message`, message =>{
     if(message.content.startsWith("help")) {
         message.channel.send("Salut, taper !help sûr #commande pour voir les commandes de Modérateur[Bêta]");
         }
-    if (message.content.startsWith(prefix + "kick")) {
-        if (message.member.roles.find('name', 'Wielder')) {
-            let sub = message.content.substring(config.prefix.length+5);
-            let args = sub.split(" ");
-            let userToKick = message.mentions.users.first();
-            var reason = sub.substring(args[0].length+1);
-            message.guild.member(userToKick).kick();
-            message.guild.channels.find('name', 'mod-log').sendMessage("**User has felt the power of Hammer!** \n**User that was kicked**: " + userToKick + "\n**Reason**: " + reason + "\n**Date**: " + today + "\n**Time of Action**: " + time + "\n**Responsible Wielder**: " + message.author + "\n**Channel**: " + message.channel.name);
-        }
-    }
+    if(message.content.startsWith(prefix + "help")) {
+        (message.guild.reateChannel('ticket-' + (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4), 0, 'New ticket', bot.config.category).then((channel) => {
+          r.table('tickets').insert({
+            id: msg.author.id,
+            case: (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4),
+            channel: channel.id
+          }).run();
+          channel.createMessage({
+            embed: {
+              description: 'ID: ' + (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4) + '\nMessage: ' + (args.length > 0 ? args.join(' ') : 'None')
+            }
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
