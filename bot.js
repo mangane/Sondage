4df1c9446bc9d57b342313ab6b73a0e023f4b372
mangane <40431348+mangane@users.noreply.github.com>
2019-03-09 04:05:07 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,16 @@ client.on(`message`, message =>{
     if(message.content.startsWith("help")) {
         message.channel.send("Salut, taper !help sûr #commande pour voir les commandes de Modérateur[Bêta]");
         }
+    if (message.content.startsWith(prefix + "kick")) {
+        if (message.member.roles.find('name', 'Wielder')) {
+            let sub = message.content.substring(config.modprefix.length+5);
+            let args = sub.split(" ");
+            let userToKick = message.mentions.users.first();
+            var reason = sub.substring(args[0].length+1);
+            message.guild.member(userToKick).kick();
+            message.guild.channels.find('name', 'mod-log').sendMessage("**User has felt the power of Hammer!** \n**User that was kicked**: " + userToKick + "\n**Reason**: " + reason + "\n**Date**: " + today + "\n**Time of Action**: " + time + "\n**Responsible Wielder**: " + message.author + "\n**Channel**: " + message.channel.name);
+        }
+    }
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
