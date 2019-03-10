62bab6cf17cc31459373a7b828844dd65f40b2ec
mangane <40431348+mangane@users.noreply.github.com>
2019-03-10 02:44:44 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,20 @@ client.on(`message`, message =>{
         message.delete()
         message.channel.send("Salut, taper !help sûr #commande pour voir les commandes de Modérateur[Bêta]");
         }
+    /*Kick*/
+client.on('message',message =>{
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLocaleLowerCase() === prefix + 'kick'){
+       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
+       let member = message.mentions.members.first()
+       if (!member) return message.channel.send("Veuillez mentionner un utilisateur :x:")
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas kick cet utilisateur :x:")
+       if (!member.kickable) return message.channel.send("Je ne peux pas exclure cet utilisateur :sunglass:")
+       member.kick()
+       message.channel.send("**"+member.user.username + '** a été exclu :white_check_mark:')
+    }
     
 });
 // THIS  MUST  BE  THIS  WAY
