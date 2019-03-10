cb544c9ef0ee62de3e21195269cf11affbc43b7f
mangane <40431348+mangane@users.noreply.github.com>
2019-03-10 05:43:17 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,15 +17,13 @@ client.on(`message`, message =>{
         }
     if(message.content.startsWith(prefix + "say")) {
         message.channel.send("${args.join}")
-        }
     
-    Client.on("guildMemberAdd", member => {
-   member.guild.defaultChannel.send("Welcome to: " + member.guild.name + " Hope you enjoy it here")
-});
-
-Client.on("guildMemberRemove", member => {
-   member.guild.defaultChannel.send("Goodbye: " + member.user.username + " from " + member.guild.name)
-});
+        if (message.content.startsWith(prefix + "say") {
+		message.delete()
+        const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setDescription(message.author.username + " says: " + args.join(" "));
+		message.channel.send({embed})
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
