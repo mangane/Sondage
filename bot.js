846ce83c5737d2291a9d30d6897568198e6a2326
mangane <40431348+mangane@users.noreply.github.com>
2019-03-10 04:49:52 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,9 +22,9 @@ client.on(`message`, message =>{
 		const embed = new Discord.RichEmbed()
 		.setColor(0x954D23)
 		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
+		.addField("!help", "Affiche les commandes")
+		.addField("!info", "Donne des informations sur le bot")
+		.addField("!invite", "Donne le lien pour me faire joindre votre serveur")
 		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
 		.addField("!cat", "Will send a random cat image");
 		message.channel.send({embed})
