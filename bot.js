d3775f0a620fc4ebaf4126b3a6a749f40d3a8093
mangane <40431348+mangane@users.noreply.github.com>
2019-03-10 04:44:08 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,6 +18,17 @@ client.on(`message`, message =>{
     if(message.content.startsWith(prefix + "say")) {
         message.channel.send("${args.join}")
         }
+    if (command == "help") {
+		const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setTitle("Command List:")
+		.addField("!help", "Will give the current command list")
+		.addField("!ping", "WIll show the ping time for the bot")
+		.addField("!say [text]", "Will make the bot say something")
+		.addField("!announcement [text]", "Will make the bot say an announcement and tag everyone")
+		.addField("!cat", "Will send a random cat image");
+		message.channel.send({embed})
+	}
     
 });
 // THIS  MUST  BE  THIS  WAY
