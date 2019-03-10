626f90686c32212f1bcfa6d562973e94877ddf60
mangane <40431348+mangane@users.noreply.github.com>
2019-03-10 05:59:26 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,13 +16,7 @@ client.on(`message`, message =>{
         message.channel.send(`${message.author.username} Salut, taper !help sûr #commande pour voir les commandes de Modérateur[Bêta]`);
         }
     if(message.content.startsWith(prefix + "say")) {
-        message.channel.send("${args.join}")
-    
-        if (message.content.startsWith(prefix + "say") {
-		message.delete()
-        message.channel.send(args.join(" "));
-		message.channel.send({embed}
-	    }
+        message.channel.send("args.join(" "))
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
