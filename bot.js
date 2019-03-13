96030aab91a537b270accb6506ae6e02309dd011
mangane <40431348+mangane@users.noreply.github.com>
2019-03-13 14:19:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const CLEAR_MESSAGES = '!clearMessages';
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "!";
+var prefix = "&";
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -11,7 +11,7 @@ client.on("ready", () => {
 });
 client.on("ready", () => {
 console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-client.user.setActivity(`Overbot Maintenance`);
+client.user.setActivity(`Patch de OverBot`);
 });
 
 client.on(`message`, message =>{
@@ -19,9 +19,13 @@ client.on(`message`, message =>{
         message.delete()
         message.channel.send(`${message.author.username} Salut, taper !help sûr #commande pour voir les commandes de Modérateur[Bêta]`);
         }
-    
-    
-       
+if (message.content.startsWith(prefix + "help")) {		
+     const embed = new Discord.RichEmbed()		
+     .setColor(0x954D23)		
+     .setTitle("Patch OverBot V0.5.0")		
+     .addfield("1","Modification du préfix "!" en "&"")
+     	message.channel.send({embed})	}
+}
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
