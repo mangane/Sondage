0c4aafc8641d64c0e81b7bb2dd25e7262e51ad84
mangane <40431348+mangane@users.noreply.github.com>
2019-03-15 17:49:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,15 +15,15 @@ client.user.setActivity(`Patch de OverBot`);
 });
 
 client.on(`message`, message =>{
-if(message.content.startsWith(prefix + "info")) {		
+if(message.content.startsWith(prefix + "patch")) {		
      const embed = new Discord.RichEmbed()		
      .setColor(0x954D23)		
      .setTitle("Disponibilité du bot :")		 
      .addField("Mise à jour du 13/03/19 :","Suite à la demande de plusieurs utilisateurs, le statu du bot est maintenant : joue à taper &help +nombre de serveurs")
      .addField("Commande retiré :","La commande &ping à été retiré pour une courte durée (elle reviendra normalement à la prochaine mise à jour")
-     .addField("mention","@everyone")
+     .addField("mention","@Notife")
      message.channel.send({embed})
-    message.channel.send("${message.mentions.users.first()}")
+    message.channel.send(`${message.mentions.users.first()}`)
     }
 
 });
