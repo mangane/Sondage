3cfd3766e4ba29140d38fe432f97512ceca81bc8
mangane <40431348+mangane@users.noreply.github.com>
2019-03-15 10:34:00 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ if(message.content.startsWith(prefix + "patch")) {
      message.channel.send({embed})
     message.channel.send(`${message.mentions.roles.first()}`)
     }
-    if (message.content.startsWith(prefix + 'say')) {
+    if(message.content.startsWith(prefix + 'say')) {
 		let m = args.slice(1).join(' ');
     message.delete(100);
     message.channel.send(`${m}`);
