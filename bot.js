06f8f01b2c083aa446da9386b63caff11f8a19e5
mangane <40431348+mangane@users.noreply.github.com>
2019-03-15 18:47:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,20 +13,20 @@ client.on("ready", () => {
 console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 client.user.setActivity(`Patch de OverBot`);
 });
+
 client.on(`message`, message =>{
-    if(message.content.startsWith(prefix + "patch")) {
-        message.delete()
-        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas accès à cette commande, seul les administrateur on accès à cette commande!");
+if(message.content.startsWith(prefix + "patch")) {		
      const embed = new Discord.RichEmbed()		
      .setColor(0x954D23)		
-     .setTitle("Patch/Mise à jour :")		 
+     .setTitle("Disponibilité du bot :")		 
      .addField("Mise à jour du 13/03/19 :","Suite à la demande de plusieurs utilisateurs, le statu du bot est maintenant : joue à taper &help +nombre de serveurs")
      .addField("Commande retiré :","La commande &ping à été retiré pour une courte durée (elle reviendra normalement à la prochaine mise à jour")
-     .addField("mention","@Notife")
      .addField("Mise à jour du 15/03/19","Une commande à été rajouter, la commande &sondage")
-     .setFooter(`Bot crée par ⏳Gaétan#2852`)
+     .addField("mention","@Notife")
      message.channel.send({embed})
     message.channel.send(`${message.mentions.roles.first()}`)
-    });
+    }
+
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
