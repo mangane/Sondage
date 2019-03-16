9214e9e37579694ea65359057cb5fc513edcded3
HommeDeSable <48624943+HommeDeSable@users.noreply.github.com>
2019-03-16 17:45:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on(`message`, message =>{
      .addField("Mise à jour du 13/03/19 :","Suite à la demande de plusieurs utilisateurs, le statu du bot est maintenant : joue à taper &help +nombre de serveurs")
      .addField("Commande retiré :","La commande &ping à été retiré pour une courte durée (elle reviendra normalement à la prochaine mise à jour")
      .addField("Mise à jour du 15/03/19","deux commandes ont été rajouter, la commande &sondage, la commande &say")
-     .addField("mention",`${message.mentions.roles.first()}`)
+     .addField("mention",`${message.mentions.roles.first()}`);
      message.channel.send({embed})
     message.channel.send(`${message.mentions.roles.first()}`)
     }
