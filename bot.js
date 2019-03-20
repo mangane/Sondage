0170b913f823ab318b2e0347e5590670350c12e6
SokanLeS <48765683+SokanLeS@users.noreply.github.com>
2019-03-20 09:34:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on("ready", () => {
 client.on("ready", () => {
 console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 client.user.setActivity(`La Nation Bot`);
-});
+client.on(`message`, message =>{
 if(message.content.startsWith(prefix + "kick")) {
         if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR"))
             return message.channel.send("Vous n'avez pas accès à cette commande, seul les administrateur on accès à cette commande!");
@@ -21,6 +21,7 @@ if(message.content.startsWith(prefix + "kick")) {
         const member = message.mentions.members.first();
         message.channel.send(`${message.mentions.users.first()} à été Kické par ${message.author.username}`);
         member.kick();
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
